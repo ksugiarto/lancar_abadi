6d6447fc253e80a214b9dec2bad717b2c416ea5d
Kristono Sugiarto <kristono.sugiarto@gmail.com>
2014-04-17 15:42:17 +0700

    Fixing again manage leave day feature.

--- app/controllers/emp_months_controller.rb
+++ app/controllers/emp_months_controller.rb
@@ -40,28 +40,54 @@ class EmpMonthsController < ApplicationController
     (1..(1.month.from_now.end_of_month.strftime("%d")).to_i).each do |date| # looping all next month's days
       @emp_date = @emp_month.dates.create(:date_order => date.to_i)
       Employee.where("grade IN (1,2)").order("RANDOM()").each do |emp| # looping all employees in random order
-        if emp.grade.to_i==1 && emp.total_leave_day.to_i<4 && @emp_date.total_top_grade.to_i==0 && @emp_date.total_emp.to_i<2 && date.to_i-emp.last_leave_day.to_i>5
-          if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
-            @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
-            @emp_date.details.create(:employee_id => emp.id)
-            emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
-          elsif emp.gender.to_i==1
-            @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
-            @emp_date.details.create(:employee_id => emp.id)
-            emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+
+        if "#{Date.today.strftime('%Y')}-#{1.month.from_now.strftime("%m")}-#{date}".to_date.sunday? # checking day
+          if emp.grade.to_i==1 && emp.total_leave_day.to_i<4 && @emp_date.total_top_grade.to_i==0 && @emp_date.total_emp.to_i<3 && date.to_i-emp.last_leave_day.to_i>5
+            if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
+              @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            elsif emp.gender.to_i==1
+              @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            end
+          elsif emp.grade.to_i==2 && emp.total_leave_day.to_i<4 && @emp_date.total_emp.to_i<3 && date.to_i-emp.last_leave_day.to_i>5
+            if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
+              @emp_date.update_attributes(:female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            elsif emp.gender.to_i==1
+              @emp_date.update_attributes(:total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            end
+          else
           end
-        elsif emp.grade.to_i==2 && emp.total_leave_day.to_i<4 && @emp_date.total_emp.to_i<2 && date.to_i-emp.last_leave_day.to_i>5
-          if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
-            @emp_date.update_attributes(:female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
-            @emp_date.details.create(:employee_id => emp.id)
-            emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
-          elsif emp.gender.to_i==1
-            @emp_date.update_attributes(:total_emp => @emp_date.total_emp.to_i+1)
-            @emp_date.details.create(:employee_id => emp.id)
-            emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+        else # checking day
+          if emp.grade.to_i==1 && emp.total_leave_day.to_i<4 && @emp_date.total_top_grade.to_i==0 && @emp_date.total_emp.to_i<2 && date.to_i-emp.last_leave_day.to_i>5
+            if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
+              @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            elsif emp.gender.to_i==1
+              @emp_date.update_attributes(:total_top_grade => @emp_date.total_top_grade.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            end
+          elsif emp.grade.to_i==2 && emp.total_leave_day.to_i<4 && @emp_date.total_emp.to_i<2 && date.to_i-emp.last_leave_day.to_i>5
+            if emp.gender.to_i==2 && @emp_date.female_emp.to_i==0
+              @emp_date.update_attributes(:female_emp => @emp_date.female_emp.to_i+1, :total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            elsif emp.gender.to_i==1
+              @emp_date.update_attributes(:total_emp => @emp_date.total_emp.to_i+1)
+              @emp_date.details.create(:employee_id => emp.id)
+              emp.update_attributes(:last_leave_day => date, :total_leave_day => emp.total_leave_day.to_i+1)
+            end
+          else
           end
-        else
-        end
+        end # checking day
       end # looping all employees in random order
     end # looping all next month's days
 
